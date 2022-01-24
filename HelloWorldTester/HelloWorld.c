ca8b7be45d7f9f1095ad3b573b7c7bc9a2d7927f
Henry Kiechlin <kiechlinh@gmail.com>
2022-01-23 21:25:01 -0500

    init

---
+++ HelloWorldTester/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+     char test[] = "My friend John is real cool!";
+     printf("%s",test);
+
+     return 0;
+}
--- HelloWorldTester/HelloWorld.cc
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-int main() {
-     string H = "Hello World. My friend John is sitting next to me! I dont know if he is reading this tho";
-     cout << H;
-}
