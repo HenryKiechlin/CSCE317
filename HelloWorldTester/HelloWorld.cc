9364b85f75489a66139d93815ec50e5748bb40ac
Henry Kiechlin <kiechlinh@gmail.com>
2022-01-23 20:56:17 -0500

    init

--- HelloWorldTester/HelloWorld.cc
+++ HelloWorldTester/HelloWorld.cc
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main() {
-     string H = "Hello World";
+     string H = "Hello World. My friend John is sitting next to me! I dont know if he is reading this tho";
      cout << H;
 }
